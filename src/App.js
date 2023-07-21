71c760eb02f8e11d7a31fdd66d8a60d94ae403c5
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-07-21 06:16:50 +0300

    refactor project to start working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {solid} from "@fortawesome/fontawesome-svg-core/import.macro";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export const Mailroom = () => (
+    <div>
+        <FontAwesomeIcon icon={solid("heart")} style={{color: "#ff0000",}} />
     </div>
-  );
-}
+)
 
-export default App;
+export default Mailroom;
