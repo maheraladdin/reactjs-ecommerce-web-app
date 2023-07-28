a76a6aa68ab0fe24a5abc1912965e47cb1ba2a49
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-07-28 12:14:00 +0300

    UPD UserChangePassword.jsx & package.json

--- src/components/User/UserChangePassword.jsx
+++ src/components/User/UserChangePassword.jsx
@@ -10,15 +10,15 @@ export default function UserChangePassword() {
 			</h3>
 			<Form.Group controlId={"CurrentPassword"}>
 				<Form.Label>Current Password</Form.Label>
-				<Form.Control type={"text"} placeholder={"Current Password"} />
+				<Form.Control type={"password"} placeholder={"Current Password"} />
 			</Form.Group>
 			<Form.Group controlId={"NewPassword"}>
 				<Form.Label>New Password</Form.Label>
-				<Form.Control type={"text"} placeholder={"New Password"} />
+				<Form.Control type={"password"} placeholder={"New Password"} />
 			</Form.Group>
 			<Form.Group controlId={"ConfirmNewPassword"}>
 				<Form.Label>New Password Confirmation</Form.Label>
-				<Form.Control type={"text"} placeholder={"New Password Confirmation"} />
+				<Form.Control type={"password"} placeholder={"New Password Confirmation"} />
 			</Form.Group>
 			<Button className="align-self-end" variant={"primary"}>
 				Update Password
