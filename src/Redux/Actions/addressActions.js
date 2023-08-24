3abe0f969d5a631ee1528e7ac635b754b6ea7e7b
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-08-24 22:51:25 +0300

    UPD addressActions.js

--- src/Redux/Actions/addressActions.js
+++ src/Redux/Actions/addressActions.js
@@ -12,34 +12,47 @@ export const getAddresses = (params) => reduxApi("get", "/addresses", params,
         })
     });
 
+export const getAddressById = (address, params) => reduxApi("get", `/addresses/${address}`, params,
+    (dispatch,payload) => {
+        dispatch({
+            type: GET_ADDRESSES,
+            payload: {
+                address: payload.data.address,
+                status: payload.status,
+            }
+        })
+    });
+
 
 export const createAddress = (params) => reduxApi("post", "/addresses", params,
     (dispatch,payload) => {
         dispatch({
             type: CREATE_ADDRESS,
             payload: {
+                addresses: payload.data.addresses,
                 status: payload.status,
             }
         })
     });
 
 
-export const updateAddress = (params) => reduxApi("put", "/addresses", params,
+export const updateAddress = (address, params) => reduxApi("put", `/addresses/${address}`, params,
     (dispatch,payload) => {
         dispatch({
             type: UPDATE_ADDRESS,
             payload: {
+                addresses: payload.data.addresses,
                 status: payload.status,
-                address: payload.data.address,
             }
         })
     });
 
-export const deleteAddress = (params) => reduxApi("delete", "/addresses", params,
+export const deleteAddress = (address, params) => reduxApi("delete", `/addresses/${address}`, params,
     (dispatch,payload) => {
         dispatch({
             type: DELETE_ADDRESS,
             payload: {
+                addresses: payload.data.addresses,
                 status: payload.status,
             }
         })
