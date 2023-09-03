9d32d1643a948641b9ce77a7ac61bcb243f53725
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-09-03 04:29:56 +0300

    UPD ProductCardTemplate.jsx

--- src/components/product/ProductCardTemplate.jsx
+++ src/components/product/ProductCardTemplate.jsx
@@ -10,28 +10,25 @@ export function ProductCardTemplate({product,children}) {
 			<Link to={productDetailsRoute.replace(":id",product._id)} className="text-decoration-none text-dark">
 				<Card.Img
 					style={{
-						height: "200px",
 						objectFit: "contain"
 					}}
 					variant="top" src={product.imageCover} />
 				<Card.Body className="d-flex flex-column justify-content-between" style={{zIndex: 2}}>
 					<Card.Title className={"text-truncate"}>{product.title}</Card.Title>
 					<Card.Text className={"text-truncate"}>{product.description}</Card.Text>
-					<section className="d-flex justify-content-between align-items-center gap-5">
-						<section className="text-end">
-							<i className="fa-solid fa-star me-1 text-warning"></i>
-							{product.ratingsAverage}
-						</section>
-						<section className="h5 fw-light mb-0">
-							{
-								product.discountedPrice ?
-									<>
-										<Badge bg={"danger"} className={"me-2"} ><del>{product.price}$</del></Badge>
-										<Badge>{product.discountedPrice}$</Badge>
-									</>
-									: <Badge>{product.price}$</Badge>
-							}
-						</section>
+					<section className="text-end">
+						<i className="fa-solid fa-star me-1 text-warning"></i>
+						{product.ratingsAverage}
+					</section>
+					<section className="h5 fw-light mb-0">
+						{
+							product.discountedPrice ?
+								<>
+									<Badge bg={"danger"} className={"me-2"} ><del>{product.price}$</del></Badge>
+									<Badge>{product.discountedPrice}$</Badge>
+								</>
+								: <Badge>{product.price}$</Badge>
+						}
 					</section>
 				</Card.Body>
 			</Link>
